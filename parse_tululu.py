8e31fb4962230c7d3d819baec406e2e78dde3810
DevmanYur <devmanyur@gmail.com>
2024-05-26 09:43:45 +0300

    Запрос для текста и запрос для картинки перенес в функцию download_txt и download_image

--- parse_tululu.py
+++ parse_tululu.py
@@ -21,41 +21,37 @@ def get_soup(url, page):
     return soup
 
 
-def get_response_txt_image(url, page, image_tag):
-    url_txt = f"{url}/txt.php"
-    payload_txt = {'id': page}
-    response_txt = requests.get(url_txt, params=payload_txt)
-    response_txt.raise_for_status()
-    check_for_redirect(response_txt)
-
-    url_image = f'{url}{image_tag}'
-    response_image = requests.get(url_image)
-    response_image.raise_for_status()
-    check_for_redirect(response_image)
-
-    return response_txt, response_image
-
-
 def check_for_redirect(response):
             if response.history:
                 raise HTTPError
 
 
-def download_txt(url, filename, folder='books/'):
+def download_txt(url, page, filename, folder='books/'):
+    url_txt = f"{url}/txt.php"
+    payload_txt = {'id': page}
+    response = requests.get(url_txt, params=payload_txt)
+    response.raise_for_status()
+    check_for_redirect(response)
+
     Path(folder).mkdir(parents=True, exist_ok=True)
     filepath_without_format = os.path.join(folder, sanitize_filename(filename))
     filepath = f'{filepath_without_format}.txt'
     with open(filepath, 'w') as file:
-        file.write(url.text)
+        file.write(response.text)
 
     return filepath
 
 
-def download_images(url, filename, folder='images/'):
+def download_images(url, image_tag, filename, folder='images/'):
+    url_image = f'{url}{image_tag}'
+    response = requests.get(url_image)
+    response.raise_for_status()
+    check_for_redirect(response)
+
     Path(folder).mkdir(parents=True, exist_ok=True)
     filepath = os.path.join(folder, sanitize_filename(filename))
     with open(filepath, 'wb') as file:
-        file.write(url.content)
+        file.write(response.content)
 
     return filepath
 
@@ -108,9 +104,8 @@ def main():
         try:
             soup = get_soup(url, page)
             tittle, author, comments, genres, image, image_tag = parse_book_page(soup)
-            response_txt, response_image = get_response_txt_image(url, page, image_tag)
-            download_txt(response_txt, f'{page}. {tittle}')
-            download_images(response_image, image)
+            download_txt(url, page, f'{page}. {tittle}')
+            download_images(url, image_tag, image)
             download_comments(comments, f'{page}. {tittle} - комментарии')
         except HTTPError:
             logger.warning(f'Страница {url}/b{page} не существует')
